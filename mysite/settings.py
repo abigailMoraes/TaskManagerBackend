dbc876efa288286a0b3abd703a763e740d8a257a
l8k1b <abigail.theresa.moraes@gmail.com>
2023-04-08 16:17:41 -0700

    Added Models, Serializers, Urls for taskManagerApp and packages to requirements.txt

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,9 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taskManager',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
---
+++ taskManager/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+from taskManager.models import User, Event, Task
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username',
+                  'firstname',
+                  'lastname',
+                  'email',
+                  'phone')
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ( 
+                  'eventID',
+                  'parentTaskID',
+                  'title', 
+                  'eventdescription',
+                  'end',
+                  'start',
+                  'allDay',
+                  'color',
+                  'draggable',
+                  'username',
+                  'completed',
+                  )
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = (
+                'taskID',
+                'title',
+                'taskdescription',
+                'end',
+                'start',
+                'deadline',
+                'repeating',
+                'completed',
+                'hours',
+                'priority',
+                'username',
+                'allDay',
+                'color',
+                'scheduled',
+                'draggable',
+                )
